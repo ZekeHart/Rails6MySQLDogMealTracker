8830fcc71818668458357f0b4f7c6761bd232b36
Zeke Hart <heirloch@gmail.com>
2021-05-05 20:36:16 -0400

    First path changes to pages. Sending some to /admin and some to /reports

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
-  resources :meals
-  resources :dogs
-  resources :owners
+  resources :meals, path: 'admin/meals'
+  resources :dogs, except: :show, path: 'admin/dogs'
+  resources :dogs, only: :show, path: 'reports/dogs'
+  resources :owners, except: :owner_report, path: 'admin/owners'  
   root 'sessions#welcome'
   resources :users, only: [:new, :create]
-  get 'owner_report', to: 'owners#owner_report'
+  get 'reports/owner_report', to: 'owners#owner_report'
   get 'login', to: 'sessions#login', as: 'login'
   post 'login', to: 'sessions#create'
   delete '/sessions', to: 'sessions#destroy'
